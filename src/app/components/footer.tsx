1074a7f31a95821d433960f7a483060a7734c9e8
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-23 18:48:18 +0900

    add header, footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className={`mt-auto p-4`}>&copy; All rights reserved.</div>;
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="bg-neutral-950 p-4">
+      <h1 className="text-xl">Header</h1>
+    </div>
+  );
+}
