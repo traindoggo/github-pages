70910245ae08daaf24f0f61e34086ef880d33fc2
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-23 18:47:11 +0900

    add layout, container

---
+++ src/app/components/container.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      className={`min-h-screen flex flex-col
+      bg-neutral-950 text-neutral-200`}
+    >
+      {children}
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import Container from "./components/container";
+import Footer from "./components/footer";
+import Header from "./components/header";
 import "./styles/tailwind.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} min-h-screen`}>
+        <Container>
+          <Header />
+          {children}
+          <Footer />
+        </Container>
+      </body>
     </html>
   );
 }
