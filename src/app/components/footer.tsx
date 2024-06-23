e8f5f2425b27db872738c7afe924eeec4c6bac77
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-23 19:01:42 +0900

    fix header, footer css

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,13 @@
 export default function Footer() {
-  return <div className={`mt-auto p-4`}>&copy; All rights reserved.</div>;
+  return (
+    <div
+      className={`mt-auto p-4
+      text-center
+      border-t-4 border-neutral-900`}
+    >
+      <span className="text-sm text-neutral-400">
+        Traindoggo &copy; All rights reserved.
+      </span>
+    </div>
+  );
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,6 +1,9 @@
 export default function Header() {
   return (
-    <div className="bg-neutral-950 p-4">
+    <div
+      className="p-4
+    bg-neutral-950 border-b-4 border-neutral-900"
+    >
       <h1 className="text-xl">Header</h1>
     </div>
   );
