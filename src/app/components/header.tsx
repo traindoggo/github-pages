75917bfe9ad9dad392361472482abb6538004b16
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-10 20:26:47 +0900

    components/header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,38 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    title: "Top",
+    href: "/",
+  },
+  {
+    title: "Blogs",
+    href: "/blogs",
+  },
+];
+
 export default function Header() {
+  const pathname = usePathname();
+
   return (
-    <div
-      className="p-4
-    bg-neutral-950 border-b-4 border-neutral-900"
+    <header
+      className="flex gap-4 p-4 bg-neutral-950 border-neutral-800
+      border-b-4"
     >
-      <h1 className="text-xl">Header</h1>
-    </div>
+      {links.map((link) => (
+        <Link
+          key={link.title}
+          className={`${
+            pathname == link.href ? "text-neutral-50" : "text-neutral-700"
+          } duration-150`}
+          href={link.href}
+        >
+          {link.title}
+        </Link>
+      ))}
+    </header>
   );
 }
