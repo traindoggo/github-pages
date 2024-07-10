57fd48fa53b74dea345d7a06ea95d013486bebe3
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-10 20:27:00 +0900

    top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ const BASE_PATH = nextConfig.basePath || "";
 
 export default function Home() {
   return (
-    <main className="flex-1 bg-neutral-950 p-4">
-      <h1 className="text-xl">Welcome Home</h1>
+    <main className={`flex-1 p-4 bg-neutral-950`}>
+      <h1 className={`text-xl`}>Top Page</h1>
     </main>
   );
 }
