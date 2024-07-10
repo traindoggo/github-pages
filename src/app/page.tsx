cf685bd064489acff4d859f2f368c3ae838b3f1b
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-10 21:09:48 +0900

    add animation and title css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,15 @@ const BASE_PATH = nextConfig.basePath || "";
 
 export default function Home() {
   return (
-    <main className={`flex-1 p-4 bg-neutral-950`}>
-      <h1 className={`text-xl`}>Top Page</h1>
+    <main className={`flex-1 flex flex-col pt-6 p-10 bg-neutral-950`}>
+      <header className={`p-4 flex justify-center`}>
+        <div className={`flex gap-6 animate-text-focus-in`}>
+          <h1 className={`text-4xl`}>Hello World</h1>
+          <span className={`text-4xl`}>:^&#41;</span>
+        </div>
+      </header>
+
+      <div className={`flex-1`}></div>
     </main>
   );
 }
