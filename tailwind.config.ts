91a54bb7a793bbbc6c3e51087adff14ae2ba0bf2
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-10 21:06:15 +0900

    add tailwind animation: text-focus-in using blur :^)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "text-focus-in":
+          "text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both",
+      },
+      keyframes: {
+        "text-focus-in": {
+          "0%": {
+            filter: "blur(12px)",
+            opacity: "0",
+          },
+          to: {
+            filter: "blur(0)",
+            opacity: "1",
+          },
+        },
       },
     },
   },
