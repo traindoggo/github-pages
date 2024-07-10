615f89572763d291049c1476bb687f391cc51ee6
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-10 20:21:30 +0900

    statis seeds

---
+++ src/db/seed.ts
@@ -0,0 +1,17 @@
+export const blogs: BlogSchema[] = [
+  {
+    id: 1,
+    title: "first blog",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dolorem, rem ex placeat earum fuga tenetur similique ipsa culpa dolor aspernatur voluptatem quaerat quisquam, neque praesentium at magni. Sequi, quae.`,
+  },
+  {
+    id: 2,
+    title: "second blog",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dolorem, rem ex placeat earum fuga tenetur similique ipsa culpa dolor aspernatur voluptatem quaerat quisquam, neque praesentium at magni. Sequi, quae.`,
+  },
+  {
+    id: 3,
+    title: "third blog",
+    content: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id optio animi porro asperiores, ab enim, perspiciatis minima harum nesciunt magni impedit nostrum ducimus architecto illum quod. Vel, tenetur perferendis! Ipsa ratione, vero delectus quaerat nobis molestias. Ex expedita ab quas distinctio quibusdam, et provident maxime illum, magni enim, delectus laboriosam?`,
+  },
+];
